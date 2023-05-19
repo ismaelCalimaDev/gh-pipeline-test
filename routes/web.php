80fb7e8b4fb86a41dc680b928055b1a06f70df8c
ismaelCalimaDev <ismaelCalimaDev@users.noreply.github.com>
2023-05-19 08:11:35 +0000

    PHP Linting (Pint)

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 Route::get('/', function () {
     return \App\Models\User::query()
         ->where('name', '!=', 'Federico')
-        ->where('email', '!=', 'fede@gmail.com'  )
+        ->where('email', '!=', 'fede@gmail.com')
         ->get();
 });
