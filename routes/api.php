be15075713e46ff684ddbf8df8f055b515823711
ismaelCalimaDev <santiago@calimasolutions.com>
2023-06-05 12:31:56 +0100

    Only generate it if scribe is install

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/foo', function () {
+/*Route::get('/foo', function () {
     return response()
         ->json([
             'foo' => 'bar',
@@ -30,4 +30,4 @@ Route::get('/bar', function () {
         ->json([
             'bar' => 'foo',
         ]);
-});
+});*/
