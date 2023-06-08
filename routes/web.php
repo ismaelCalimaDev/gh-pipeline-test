ea80578851ac0872b96646ef0d18e0fc37e36e69
ismaelCalimaDev <santiago@calimasolutions.com>
2023-06-08 15:39:45 +0100

    return web php to the original state

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('welcome');
 });
