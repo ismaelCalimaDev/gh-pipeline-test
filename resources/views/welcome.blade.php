6905ca12a1e836776765609e4561b8838a954d30
ismaelCalimaDev <santiago@calimasolutions.com>
2023-06-21 12:08:52 +0100

    small cosas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,10 +133,4 @@
     </div>
 </div>
 </body>
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Kevin</title>
+</html>
