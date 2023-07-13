ea60f7f080b392f2170c4fa4288eaf5c7c9d7891
ismaelCalimaDev <ismaelCalimaDev@users.noreply.github.com>
2023-07-13 08:46:27 +0000

    PHP Linting (Pint)

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/pablo', function () {    return view('auth');})->middleware('isma');
+Route::get('/pablo', function () {
+    return view('auth');
+})->middleware('isma');
